395130d3c9511898a67e759b4efecda4626a9dbb
Thomas Maloney <ThomasTNF@live.com>
2014-02-23 02:55:39 -0600

    new file:   MTNLatin/MTNLatin.sln 	new file:   MTNLatin/MTNLatin.v12.suo 	new file:   MTNLatin/MTNLatin/App.xaml 	new file:   MTNLatin/MTNLatin/App.xaml.cs 	new file:   MTNLatin/MTNLatin/Assets/AlignmentGrid.png 	new file:   MTNLatin/MTNLatin/Assets/ApplicationIcon.png 	new file:   MTNLatin/MTNLatin/Assets/Tiles/FlipCycleTileLarge.png 	new file:   MTNLatin/MTNLatin/Assets/Tiles/FlipCycleTileMedium.png 	new file:   MTNLatin/MTNLatin/Assets/Tiles/FlipCycleTileSmall.png 	new file:   MTNLatin/MTNLatin/Assets/Tiles/IconicTileMediumLarge.png 	new file:   MTNLatin/MTNLatin/Assets/Tiles/IconicTileSmall.png 	new file:   MTNLatin/MTNLatin/Bin/Debug/AppManifest.xaml 	new file:   MTNLatin/MTNLatin/Bin/Debug/Assets/ApplicationIcon.png 	new file:   MTNLatin/MTNLatin/Bin/Debug/Assets/Tiles/FlipCycleTileLarge.png 	new file:   MTNLatin/MTNLatin/Bin/Debug/Assets/Tiles/FlipCycleTileMedium.png 	new file:   MTNLatin/MTNLatin/Bin/Debug/Assets/Tiles/FlipCycleTileSmall.png 	new file:   MTNLatin/MTNLatin/Bin/Debug/Assets/Tiles/IconicTileMediumLarge.png 	new file:   MTNLatin/MTNLatin/Bin/Debug/Assets/Tiles/IconicTileSmall.png 	new file:   MTNLatin/MTNLatin/Bin/Debug/MDIL/MTNLatin.dll 	new file:   MTNLatin/MTNLatin/Bin/Debug/MDIL/WMAppManifest.xml 	new file:   MTNLatin/MTNLatin/Bin/Debug/MTNLatin.dll 	new file:   MTNLatin/MTNLatin/Bin/Debug/MTNLatin.dll.CodeAnalysisLog.xml 	new file:   MTNLatin/MTNLatin/Bin/Debug/MTNLatin.dll.lastcodeanalysissucceeded 	new file:   MTNLatin/MTNLatin/Bin/Debug/MTNLatin.pdb 	new file:   MTNLatin/MTNLatin/Bin/Debug/MTNLatin_Debug_AnyCPU.xap 	new file:   MTNLatin/MTNLatin/Bin/Debug/Properties/WMAppManifest.xml 	new file:   MTNLatin/MTNLatin/LocalizedStrings.cs 	new file:   MTNLatin/MTNLatin/MTNLatin.csproj 	new file:   MTNLatin/MTNLatin/MTNLatin.csproj.user 	new file:   MTNLatin/MTNLatin/MainPage.xaml 	new file:   MTNLatin/MTNLatin/MainPage.xaml.cs 	new file:   MTNLatin/MTNLatin/Properties/AppManifest.xml 	new file:   MTNLatin/MTNLatin/Properties/AssemblyInfo.cs 	new file:   MTNLatin/MTNLatin/Properties/WMAppManifest.xml 	new file:   MTNLatin/MTNLatin/Resources/AppResources.Designer.cs 	new file:   MTNLatin/MTNLatin/Resources/AppResources.resx 	new file:   MTNLatin/MTNLatin/Verbs.cs 	new file:   MTNLatin/MTNLatin/obj/Debug/App.g.cs 	new file:   MTNLatin/MTNLatin/obj/Debug/App.g.i.cs 	new file:   MTNLatin/MTNLatin/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   MTNLatin/MTNLatin/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   MTNLatin/MTNLatin/obj/Debug/MDIL/MDILFileList.xml 	new file:   MTNLatin/MTNLatin/obj/Debug/MDIL/MDILXapCompileLog.txt 	new file:   MTNLatin/MTNLatin/obj/Debug/MDIL/MTNLatin.dll 	new file:   MTNLatin/MTNLatin/obj/Debug/MDILCacheFile.xml 	new file:   MTNLatin/MTNLatin/obj/Debug/MTNLatin.Resources.AppResources.resources 	new file:   MTNLatin/MTNLatin/obj/Debug/MTNLatin.csproj.FileListAbsolute.txt 	new file:   MTNLatin/MTNLatin/obj/Debug/MTNLatin.csproj.GenerateResource.Cache 	new file:   MTNLatin/MTNLatin/obj/Debug/MTNLatin.dll 	new file:   MTNLatin/MTNLatin/obj/Debug/MTNLatin.g.resources 	new file:   MTNLatin/MTNLatin/obj/Debug/MTNLatin.pdb 	new file:   MTNLatin/MTNLatin/obj/Debug/MainPage.g.cs 	new file:   MTNLatin/MTNLatin/obj/Debug/MainPage.g.i.cs 	new file:   MTNLatin/MTNLatin/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   MTNLatin/MTNLatin/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   MTNLatin/MTNLatin/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   MTNLatin/MTNLatin/obj/Debug/XapCacheFile.xml

---
+++ MTNLatin/MTNLatin/MainPage.xaml.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MTNLatin.Resources;
+
+namespace MTNLatin
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Variables
+        private int conjugateN, person, princP;
+        private string verbEnd;
+        private bool getPPartSucceeded;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void btnI_Click(object sender, RoutedEventArgs e)
+        {
+            person = 0;
+            lblCurrentPerson.Text = "Person: I";
+        }
+
+        private void btnYouS_Click(object sender, RoutedEventArgs e)
+        {
+            person = 1;
+            lblCurrentPerson.Text = "Person: You";
+        }
+
+        private void btnIt_Click(object sender, RoutedEventArgs e)
+        {
+            person = 2;
+            lblCurrentPerson.Text = "Person: She, He, It";
+        }
+
+        private void btnWe_Click(object sender, RoutedEventArgs e)
+        {
+            person = 3;
+            lblCurrentPerson.Text = "Person: We";
+        }
+
+        private void btnYouP_Click(object sender, RoutedEventArgs e)
+        {
+            person = 4;
+            lblCurrentPerson.Text = "Person: You (Plural)";
+        }
+
+        private void btnThey_Click(object sender, RoutedEventArgs e)
+        {
+            person = 5;
+            lblCurrentPerson.Text = "Person: They";
+        }
+
+        private string findVerbEnding()
+        {
+            string verb = lblCurrentVerb.Text;
+            try
+            {
+                string ppart = verb.Substring(verb.Length - 3, 3);
+                getPPartSucceeded = true;
+                return ppart;
+            }
+            catch (Exception)
+            {
+                Exception up = new Exception();
+                getPPartSucceeded = false;
+                MessageBox.Show("ERROR");
+                throw up;       // huehuehue
+            }
+        }
+
+        private void btnConj_Click(object sender, RoutedEventArgs e)
+        {
+            findVerbEnding();
+            if (getPPartSucceeded)
+            {
+                string conjugate = conj();
+                txtVerb.Text = conjugate;
+            }
+            else if (!getPPartSucceeded)
+            {
+                lblCurrentVerb.Text = "None";
+                txtVerb.Text = "Error: Invalid Input (Probably not supported, will most-likely be soon)";
+            }
+        }
+
+        private void txtVerb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            lblCurrentVerb.Text = txtVerb.Text;
+        }
+
+        private string conj()
+        {
+            Verbs verbs = new Verbs();
+            verbEnd = findVerbEnding();
+            /*switch (verbEnd)
+            {
+                case "are":
+                    conjugateN = 0;
+                    break;
+                case "ere":
+                    conjugateN = 1;     // It could be two, but it's 2:16am, cut me some slack
+                    break;
+                case "ire":
+                    conjugateN = 3;
+                    break;
+                default:
+                    break;
+            }*/
+            verbs.initialize(conjugateN, verbEnd, lblCurrentVerb.Text, lblCurrentVerb.Text, princP);
+            string people = verbs.searchArray(person);
+            string princeVal = verbs.searchMultiArray(conjugateN, princP);
+            return verbs.conjugate(people, princeVal);
+        }
+
+        private void btnFirstP_Click(object sender, RoutedEventArgs e)
+        {
+            lblPrincipalPart.Text = "PP: 1";
+            princP = 0;
+        }
+
+        private void btnSecondP_Click(object sender, RoutedEventArgs e)
+        {
+            lblPrincipalPart.Text = "PP: 2";
+            princP = 1;
+        }
+
+        private void btnThirdP_Click(object sender, RoutedEventArgs e)
+        {
+            lblPrincipalPart.Text = "PP: 3";
+            princP = 2;
+        }
+
+        private void btnFourthP_Click(object sender, RoutedEventArgs e)
+        {
+            lblPrincipalPart.Text = "PP: 4";
+            princP = 3;
+        }
+
+        private void txtVerb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtVerb.Text = string.Empty;
+        }
+
+        private void btnFirstConj_Click(object sender, RoutedEventArgs e)
+        {
+            lblConjNum.Text = "Conj: I";
+            conjugateN = 0;
+        }
+
+        private void btnSecondConj_Click(object sender, RoutedEventArgs e)
+        {
+            lblConjNum.Text = "Conj: II";
+            conjugateN = 1;
+        }
+
+        private void btnThirdConj_Click(object sender, RoutedEventArgs e)
+        {
+            lblConjNum.Text = "Conj: III";
+            conjugateN = 2;
+        }
+        private void btnThirdIOConj_Click(object sender, RoutedEventArgs e)
+        {
+            lblConjNum.Text = "Conj: III-io";
+            conjugateN = 3;
+        }
+
+        private void btnFourthConj_Click(object sender, RoutedEventArgs e)
+        {
+            lblConjNum.Text = "Conj: IV";
+            conjugateN = 4;
+        }
+
+        private void txtVerb_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                pageMain.Focus();
+            }
+
+        }
+
+        // Sample code for building a localized ApplicationBar
+        /* private void BuildLocalizedApplicationBar()
+         {
+             // Set the page's ApplicationBar to a new instance of ApplicationBar.
+             ApplicationBar = new ApplicationBar();
+
+             // Create a new button and set the text value to the localized string from AppResources.
+             ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+             appBarButton.Text = AppResources.AppBarButtonText;
+             ApplicationBar.Buttons.Add(appBarButton);
+
+             // Create a new menu item with the localized string from AppResources.
+             ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+             ApplicationBar.MenuItems.Add(appBarMenuItem);
+         }*/
+
+
+    }
+}
---
+++ MTNLatin/MTNLatin/Verbs.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTNLatin
+{
+    class Verbs
+    {
+        private int conjugationNumber;
+        private int principalPart;
+        private string infinitive;
+        private string infinitiveCut;           // The infinitive w/out the last three letters
+        private string inputString;
+        private string verbEnding;
+        private bool isInputInfinitive;
+        private string[] personEndings = new string[6] { "o", "s", "t", "mus", "tis", "nt" };         // Array 'personEndings' of size 7
+        
+        private string[,] conjEndings = new string[5,4]
+            {
+                {"o", "are","avi", "atum"},
+                {"eo", "ere", "ui", "itum"},
+                {"o", "ere", "i", "tum"},
+                {"io", "ere", "i", "tum"},
+                {"io", "ire", "ivi", "itum"}
+            };          // Two-Dimensional array 'conjEndings' (5 by 4)
+
+        enum infinitiveEndings { are, ere, ire };
+        public Verbs()
+        {
+            //TODO
+        }
+
+        public void initialize(int conjNum, string verbEnded, string verb, string infin, int principalP)
+        {
+            conjugationNumber = conjNum;
+            verbEnding = verbEnded;
+            inputString = verb;
+            infinitive = infin;
+            principalPart = principalP;
+            isInputInfinitive = true;    // As of now, it will only accept the infinitive as an input
+            //detectVerbType();          // Implement later, only regular is supported now
+            infinitiveCut = infin.Substring(0, infin.Length - 3);
+        }
+        public string conjugate(string personAdd, string principalVals)
+        {
+            //TODO
+            switch (infinitiveCut)
+            {
+                case "are":
+                    if (principalPart == 0)
+                    {
+                        infinitiveCut += personAdd + principalVals;
+                    }
+                    else
+                    {
+                        infinitiveCut += "a" + personAdd + principalVals;
+                    }
+                    break;
+                default:
+                    infinitiveCut += personAdd + principalVals;
+                    break;
+            }
+            return infinitiveCut;
+        }
+
+        public string searchMultiArray(int arrayIndexOne, int arrayIndexTwo)
+        {
+            return conjEndings[arrayIndexOne, arrayIndexTwo];
+        }
+
+        public string searchArray(int arrayIndexOne)
+        {
+            return personEndings[arrayIndexOne];
+        }
+    }
+}
